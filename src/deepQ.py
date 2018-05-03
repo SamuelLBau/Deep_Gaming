46845d6dead3d96d4ba23ed7e9ef19104c451479
SamuelLBau <sbauza@ucsd.edu>
2018-05-02 21:46:23 -0700

    Added CNN builder code

--- src/deepQ.py
+++ src/deepQ.py
@@ -34,12 +34,33 @@ K = 1 # only select an action every Kth frame, repeat prev for others
 
 class deepQ():
 
-    def __init__(self,game_env):
+    def __init__(self,game_env,board_size,n_board_frames,n_episodes=100,e=.5,CNN_struct=[]):
         self.env = game_env
-    def train(self,n_episodes=100,e=.5,CNN_struct=[]):
-        sess = tf.InteractiveSession()
-        s, readout, h_fc1=self.asri_create_network()
-        self.asri_train_network(self.env,s,readout,h_fc1,sess)
+        build_success = True
+        try:
+            self.network_input,self.network_output,self.network_readout = \
+                self.build_CNN(board_size,n_board_frames,CNN_struct)
+        except Exception as e:
+            error_message = str(e)
+            print(  "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n" + \
+                    "Error during network generation: (%s)\n"%(error_message) + \
+                    "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=")
+            build_success = False
+        
+        if build_success:
+            print("CNN built succesfully")
+        else:
+            raise Exception("Error during CNN generation, see above messages")
+    def train(self):
+        session = tf.InteractiveSession()
+
+        self.train_CNN(self.env,session,self.network_input,
+            self.network_output,self.network_readout)
+        #self.asri_train_network(self.env,self.network_input,self.network_readout,self.network_output,session)
+
+
+        #s, readout, h_fc1=self.asri_create_network()
+        #self.asri_train_network(self.env,s,readout,h_fc1,session)
         '''
         #This example runs many iterations
         for n in xrange(n_episodes):
@@ -51,17 +72,29 @@ class deepQ():
         sys.stdout.write("\n")
         sys.stdout.flush()
         '''
-    def build_CNN(self,CNN_struct):
+    def build_CNN(self,board_shape,board_frames,CNN_struct):
         '''
-            Currently, this uses exactly the net used by DeepLearningVideoGames
-            https://github.com/asrivat1/DeepLearningVideoGames/blob/master/deep_q_network.py
-            #TODO: Buildthis from arguements and test multiple different structures
+            Builds the CNN based on a designated structure
+            #TODO: write-out structure and valid inputs
+            
+            #TODO: Determine method of generating CNN_struct in above sections
+
 
         '''
-        return self.asri_create_network()
-        
-    def run(self):
+        #------------BEGIN CREATING NETWORK
+        network_input = tf.placeholder("float", [None, board_shape[0], board_shape[1], board_frames])
+
+        current_parent = network_input
+        for hidden_layer in CNN_struct[:-1]:
+            current_parent = self.assemble_layer(current_parent,hidden_layer)
+
+        network_output = current_parent
+        readout_layer = self.assemble_layer(network_output,CNN_struct[-1])
+
+        return network_input,network_output,readout_layer
+    def train_CNN(env,session,network_input,network_output,network_readout):
         pass
+    
     def asri_train_network(self,env,s,readout,h_fc1,sess):
         # define the cost function
         a = tf.placeholder("float", [None, ACTIONS])
@@ -72,6 +105,7 @@ class deepQ():
 
         # open up a game state to communicate with emulator
         game_state = env
+        game_state.reset()
 
         # store the previous observations in replay memory
         D = deque()
@@ -160,7 +194,7 @@ class deepQ():
                 if num_finished_eps%1000==0:
                     game_state.reset(render=True)
                 else:
-                    game_state.reset(render=False)
+                    game_state.reset(render=True)
             
 
             # update the old values
@@ -191,60 +225,258 @@ class deepQ():
                 h_file.write(",".join([str(x) for x in h_fc1.eval(feed_dict={s:[s_t]})[0]]) + '\n')
                 cv2.imwrite("logs_tetris/frame" + str(t) + ".png", x_t1)
             '''
-    def asri_create_network(self):
-        def weight_variable(shape):
-            initial = tf.truncated_normal(shape, stddev = 0.01)
-            return tf.Variable(initial)
-
-        def bias_variable(shape):
-            initial = tf.constant(0.01, shape = shape)
-            return tf.Variable(initial)
-
-        def conv2d(x, W, stride):
-            return tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = "SAME")
-
-        def max_pool_2x2(x):
-            return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = "SAME")
-        # network weights
-        W_conv1 = weight_variable([8, 8, 4, 32])
-        b_conv1 = bias_variable([32])
-
-        W_conv2 = weight_variable([4, 4, 32, 64])
-        b_conv2 = bias_variable([64])
-
-        W_conv3 = weight_variable([3, 3, 64, 64])
-        b_conv3 = bias_variable([64])
+    def assemble_layer(self,current_parent,layer_info):
+        cur_layer = None
         
-        W_fc1 = weight_variable([1600, 512])
-        b_fc1 = bias_variable([512])
+        #-----------------Begin helper funtions-----------------------------------------
+        def create_W(shape,std):
+            return tf.Variable(tf.truncated_normal(shape, stddev = std))
 
-        W_fc2 = weight_variable([512, ACTIONS])
-        b_fc2 = bias_variable([ACTIONS])
+        def create_B(val,shape):
+            return tf.Variable(tf.constant(val, shape = shape))
+        
+        def create_matmul(parent,W_mat):
+            return tf.matmul(parent, W_mat)
+        def create_conv2d(parent, W_mat, strides):
+            return tf.nn.conv2d(parent, W_mat, strides = strides, padding = "SAME")
+
+        def create_pool2(parent,strides,ksize):
+            return tf.nn.max_pool(parent, strides = strides,ksize = ksize, padding = "SAME")
+        
+        if "function" in layer_info:
+            layer_function = layer_info["function"]
+            function_name = layer_function["name"]
+            if function_name == "conv2d":
+                conv_stride = layer_function["stride"]
+                W_shape = layer_function["W_shape"]
+                W_std = layer_function["W_std"]
+                W_var = create_W(W_shape,W_std)
+
+                func = create_conv2d(current_parent,W_var,conv_stride)
+            elif function_name == "matmul":
+                W_shape = layer_function["W_shape"]
+                W_std   = layer_function["W_std"]
+                W_var = create_W(W_shape,W_std)
+
+                func  = create_matmul(current_parent,W_var)
+            else:
+                raise Exception(str("Unrecognized layer function (%s)"%(function_name)))
+
+        layer_type = layer_info["type"]
+        if layer_type == "pool":
+            strides=layer_info["strides"]
+            ksize=layer_info["ksize"]
+
+            cur_layer = create_pool2(current_parent,strides,ksize)
+        elif layer_type == "relu":
+            if not "function" in layer_info:
+                raise Exception("RELU Layer expects a function, none were provided")
+            try:
+                B_val = layer_info["B_val"]
+                B_shape = layer_info["B_shape"]
+                B_var = create_B(B_val,B_shape)
+            except:
+                raise Exception("Invalid/Missing values for relu bias variable")
+            #Assumes function assigned above
+            cur_layer = tf.nn.relu(func + B_var)
+        elif layer_type == "reshape":
+            shape = layer_info["shape"]
+            cur_layer = tf.reshape(current_parent, shape)
+        elif layer_type == "matmul":
+
+            W_shape = layer_info["W_shape"]
+            W_std   = layer_info["W_std"]
+            W_var = create_W(W_shape,W_std)
+
+            func  = create_matmul(current_parent,W_var)
+
+            try:
+                B_val = layer_info["B_val"]
+                B_shape = layer_info["B_shape"]
+                B_var = create_B(B_val,B_shape)
+            except:
+                raise Exception("Invalid/Missing values for relu bias variable")
+
+            cur_layer = create_matmul(current_parent,W_var) + B_var
+        else:
+            raise Exception(str("Unrecognized layer type (%s)"%(layer_type)))
+        return cur_layer
+
+def asri_network_to_CNN_struct():
+    ACTIONS = 4
+    '''
+        This builds the following CNN architecture
+        # network weights
+        W_conv1 = weight_variable([8, 8, 4, 32],.01)
+        b_conv1 = bias_variable(.01,[32])
+
+        W_conv2 = weight_variable([4, 4, 32, 64],.01)
+        b_conv2 = bias_variable(.01,[64])
+
+        W_conv3 = weight_variable([3, 3, 64, 64],.01)
+        b_conv3 = bias_variable(.01,[64])
+        
+        W_fc1 = weight_variable([1600, 512],.01)
+        b_fc1 = bias_variable(.01,[512])
+
+        W_fc2 = weight_variable([512, ACTIONS],.01)
+        b_fc2 = bias_variable(.01,[ACTIONS])
 
         # input layer
         s = tf.placeholder("float", [None, 80, 80, 4])
 
         # hidden layers
-        h_conv1 = tf.nn.relu(conv2d(s, W_conv1, 4) + b_conv1)
-        h_pool1 = max_pool_2x2(h_conv1)
-
-        h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 2) + b_conv2)
-        #h_pool2 = max_pool_2x2(h_conv2)
-
-        h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3)
-        #h_pool3 = max_pool_2x2(h_conv3)
-
-        #h_pool3_flat = tf.reshape(h_pool3, [-1, 256])
+        h_conv1 = tf.nn.relu(conv2d(s, W_conv1, [1, 4, 4, 1]) + b_conv1)
+        h_pool1 = max_pool_2x2(h_conv1,[1, 2, 2, 1],[1, 2, 2, 1])
+        h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, [1, 2, 2, 1]) + b_conv2)
+        h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, [1, 1, 1, 1]) + b_conv3)
         h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
-
         h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
 
         # readout layer
         readout = tf.matmul(h_fc1, W_fc2) + b_fc2
+    '''
 
-        return s, readout, h_fc1
+    #W_conv1 = weight_variable([8, 8, 4, 32],.01)
+    #b_conv1 = bias_variable(.01,[32])
+    #h_conv1 = tf.nn.relu(conv2d(s, W_conv1, [1, 4, 4, 1]) + b_conv1)
+    layer_1 = {}
+    layer_1["type"]     = "relu"
+    layer_1["B_val"]    = .01
+    layer_1["B_shape"]  = [32]
+
+    layer_1["function"]             = {}
+    layer_1["function"]["name"]     = "conv2d"
+    layer_1["function"]["stride"]   = [1, 4, 4, 1]
+    layer_1["function"]["W_shape"]  = [8, 8, 4, 32]  
+    layer_1["function"]["W_std"]    = .01
+
+    #h_pool1 = max_pool_2x2(h_conv1,[1, 2, 2, 1],[1, 2, 2, 1])
+    layer_2 = {}
+    layer_2["type"]     = "pool"
+    layer_2["strides"]  = [1, 2, 2, 1]
+    layer_2["ksize"]    = [1, 2, 2, 1]
+
+    #W_conv2 = weight_variable([4, 4, 32, 64],.01)
+    #b_conv2 = bias_variable(.01,[64])
+    #h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, [1, 2, 2, 1]) + b_conv2)
+    layer_3 = {}
+    layer_3["type"] = "relu"
+    layer_3["B_val"]    = .01
+    layer_3["B_shape"]  = [64]
+
+    layer_3["function"] = {}
+    layer_3["function"]["name"] = "conv2d"
+    layer_3["function"]["stride"]           = [1, 2, 2, 1]
+    layer_3["function"]["W_shape"]          = [4, 4, 32, 64] 
+    layer_3["function"]["W_std"]            = .01
+
+    #W_conv3 = weight_variable([3, 3, 64, 64],.01)
+    #b_conv3 = bias_variable(.01,[64])
+    #h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, [1, 1, 1, 1]) + b_conv3)
+    layer_4 = {}
+    layer_4["type"] = "relu"
+    layer_4["B_val"]    = .01
+    layer_4["B_shape"]  = [64]
+
+    layer_4["function"] = {}
+    layer_4["function"]["name"] = "conv2d"
+    layer_4["function"]["stride"]           = [1, 1, 1, 1]
+    layer_4["function"]["W_shape"]          = [3, 3, 64, 64]
+    layer_4["function"]["W_std"]            = .01
+
+    #h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
+    layer_5 = {}
+    layer_5["type"] = "reshape"
+    layer_5["shape"]= [-1, 1600]
+
+    #W_fc1 = weight_variable([1600, 512],.01)
+    #b_fc1 = bias_variable(.01,[512])
+    #h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
+    layer_6 = {}
+    layer_6["type"] = "relu"
+    layer_6["B_val"]    = .01
+    layer_6["B_shape"]  = [512]
+
+    layer_6["function"] = {}
+    layer_6["function"]["name"] = "matmul"
+    layer_6["function"]["W_shape"]          = [1600, 512]
+    layer_6["function"]["W_std"]            = .01
+
+    #W_fc2 = weight_variable([512, ACTIONS],.01)
+    #b_fc2 = bias_variable(.01,[ACTIONS])
+    #readout = tf.matmul(h_fc1, W_fc2) + b_fc2
+    layer_7 = {}
+    layer_7["type"] = "matmul"
+    layer_7["B_val"]    = .01
+    layer_7["B_shape"]  = [ACTIONS]
+    layer_7["W_shape"]  = [512, ACTIONS]
+    layer_7["W_std"]    = .01
+
+    return [layer_1,layer_2,layer_3,layer_4,layer_5,layer_6,layer_7]
+
 if __name__ == "__main__":
     env = snake_game()
-    network = deepQ(env)
+    CNN_struct = asri_network_to_CNN_struct()
+
+    network = deepQ(env,[80,80],4,CNN_struct=CNN_struct)
     network.train()
 
+'''
+This is the example network used. It was taught to play pong, and does not perform well on snake,
+but it served as a good baseline
+
+def asri_create_network(self):
+    def weight_variable(shape,std):
+        initial = tf.truncated_normal(shape, stddev = std)
+        return tf.Variable(initial)
+
+    def bias_variable(val,shape):
+        initial = tf.constant(val, shape = shape)
+        return tf.Variable(initial)
+
+    def conv2d(parent, W_mat, strides):
+        return tf.nn.conv2d(parent, W_mat, strides = strides, padding = "SAME")
+
+    def max_pool_2x2(parent,strides,kernel):
+        return tf.nn.max_pool(parent, strides = strides,ksize = kernel, padding = "SAME")
+    # network weights
+    W_conv1 = weight_variable([8, 8, 4, 32],.01)
+    b_conv1 = bias_variable(.01,[32])
+
+    W_conv2 = weight_variable([4, 4, 32, 64],.01)
+    b_conv2 = bias_variable(.01,[64])
+
+    W_conv3 = weight_variable([3, 3, 64, 64],.01)
+    b_conv3 = bias_variable(.01,[64])
+    
+    W_fc1 = weight_variable([1600, 512],.01)
+    b_fc1 = bias_variable(.01,[512])
+
+    W_fc2 = weight_variable([512, ACTIONS],.01)
+    b_fc2 = bias_variable(.01,[ACTIONS])
+
+    # input layer
+    s = tf.placeholder("float", [None, 80, 80, 4])
+
+    # hidden layers
+    h_conv1 = tf.nn.relu(conv2d(s, W_conv1, [1, 4, 4, 1]) + b_conv1)
+
+    h_pool1 = max_pool_2x2(h_conv1,[1, 2, 2, 1],[1, 2, 2, 1])
+
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, [1, 2, 2, 1]) + b_conv2)
+    #h_pool2 = max_pool_2x2(h_conv2,[1, 2, 2, 1],[1, 2, 2, 1])
+
+    h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, [1, 1, 1, 1]) + b_conv3)
+    #h_pool3 = max_pool_2x2(h_conv3,[1, 2, 2, 1],[1, 2, 2, 1])
+
+    #h_pool3_flat = tf.reshape(h_pool3, [-1, 256])
+    h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
+
+    h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
+
+    # readout layer
+    readout = tf.matmul(h_fc1, W_fc2) + b_fc2
+
+    return s, readout, h_fc1
+    '''
